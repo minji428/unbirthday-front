9fb8698fc70bb356cb8754272638f329902df1cb
minji428 <alswl980428@gmail.com>
2022-08-01 21:07:45 +0900

    main 수정 중

--- src/component/main/main.tsx
+++ src/component/main/main.tsx
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import '../../static/main.css';
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link>
+
 
 class main extends React.Component<{}, any> {
     constructor(props: any){
@@ -11,8 +14,66 @@ class main extends React.Component<{}, any> {
 
     render() {
         return(
-            <div>
-                안녕하세요!!
+            <div style={{display:'contents'}}>
+                <div id="main" data-framer-hydrate-v2="{&quot;routeId&quot;:&quot;augiA20Il&quot;}" data-framer-ssr-released-at="2022-07-28T14:57:24.033Z" data-framer-page-optimized-at="2022-07-31T09:16:40.247Z">
+                    <div data-framer-generated="true" class="framer-6wZMz" style="display:contents">
+                        <div class="framer-72rtr7" style="min-height:100%;width:auto" name="Phone">
+                            <div style="position:absolute;pointer-events:none;user-select:none;border-radius:inherit;top:0;right:0;bottom:0;left:0" data-framer-background-image-wrapper="true">
+                                <div style="display:contents;border-radius:inherit;pointer-events:none">
+                                    <img src="https://framerusercontent.com/images/JRtUbkQg7EhALpd6pGYnBYm3b8.jpg" alt="" srcset="https://framerusercontent.com/images/JRtUbkQg7EhALpd6pGYnBYm3b8.jpg?scale-down-to=512 236w, https://framerusercontent.com/images/JRtUbkQg7EhALpd6pGYnBYm3b8.jpg?scale-down-to=1024 473w, https://framerusercontent.com/images/JRtUbkQg7EhALpd6pGYnBYm3b8.jpg?scale-down-to=2048 946w, https://framerusercontent.com/images/JRtUbkQg7EhALpd6pGYnBYm3b8.jpg 1170w" sizes="calc(100vw)" style="pointer-events:none;user-select:none;display:block;width:100%;height:100%;border-radius:inherit;object-position:center;object-fit:cover;image-rendering:auto">
+                                </div>
+                            </div>
+                                <nav class="framer-1dyra3e" name="Topbar" data-framer-name="Topbar" data-border="true">
+                                    <div class="framer-1a33id2" name="Unbirthday로고_시계_All_white" data-framer-name="Unbirthday로고_시계_All_white">
+                                        <div style="position:absolute;pointer-events:none;user-select:none;border-radius:inherit;top:0;right:0;bottom:0;left:0" data-framer-background-image-wrapper="true">
+                                            <div style="display:contents;border-radius:inherit;pointer-events:none">
+                                                <img src="https://framerusercontent.com/images/mcQFXNbLL2qghi3GAJB8gDU3WqY.png" alt="" srcset="https://framerusercontent.com/images/mcQFXNbLL2qghi3GAJB8gDU3WqY.png?scale-down-to=512 512w, https://framerusercontent.com/images/mcQFXNbLL2qghi3GAJB8gDU3WqY.png?scale-down-to=1024 1024w, https://framerusercontent.com/images/mcQFXNbLL2qghi3GAJB8gDU3WqY.png?scale-down-to=2048 2048w, https://framerusercontent.com/images/mcQFXNbLL2qghi3GAJB8gDU3WqY.png?scale-down-to=4096 4096w, https://framerusercontent.com/images/mcQFXNbLL2qghi3GAJB8gDU3WqY.png 7874w" sizes="calc(0.13157894736842105px)" style="pointer-events:none;user-select:none;display:block;width:100%;height:100%;border-radius:inherit;object-position:center;object-fit:cover;image-rendering:auto">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="framer-1p9jqg" name="Icons" data-framer-name="Icons"></div>
+                                    <div class="framer-vix6v2" name="Links" data-framer-name="Links">
+                                        <div style="outline:none;display:flex;flex-direction:column;justify-content:flex-start;flex-shrink:0;white-space:pre-wrap;word-wrap:break-word;word-break:break-word;--framer-link-hover-text-color:#666;--framer-link-current-text-color:hsl(0, 0%, 0%);--framer-paragraph-spacing:0px;--extracted-1w3ko1f:rgb(255, 255, 255);transform:none" data-framer-component-type="RichText" class="framer-p79by9 rich-text-wrapper">
+                                            <p style="--framer-line-height:1em; --framer-text-alignment:center;">
+                                                <span style="--framer-font-family:&quot;Be Vietnam Pro&quot;, serif; --framer-font-style:normal; --framer-font-weight:500; --font-selector:R0Y7QmUgVmlldG5hbSBQcm8tNTAw; --framer-text-color:var(--extracted-1w3ko1f);">TEAM</span>
+                                            </p>
+                                        </div>
+                                        <div style="outline:none;display:flex;flex-direction:column;justify-content:flex-start;flex-shrink:0;white-space:pre-wrap;word-wrap:break-word;word-break:break-word;--framer-link-hover-text-color:#666;--framer-link-current-text-color:hsl(0, 0%, 0%);--framer-paragraph-spacing:0px;--extracted-1w3ko1f:rgb(153, 153, 153);transform:none" data-framer-component-type="RichText" class="framer-1lnnpoq rich-text-wrapper">
+                                            <p style="--framer-line-height:1em; --framer-text-alignment:center;">
+                                                <span style="--framer-font-family:&quot;Be Vietnam Pro&quot;, serif; --framer-font-style:normal; --framer-font-weight:500; --font-selector:R0Y7QmUgVmlldG5hbSBQcm8tNTAw; --framer-text-color:var(--extracted-1w3ko1f);">JOIN</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </nav>
+                                <div style="outline:none;display:flex;flex-direction:column;justify-content:flex-start;flex-shrink:0;white-space:pre-wrap;word-wrap:break-word;word-break:break-word;--framer-link-text-color:rgb(0, 153, 255);--framer-link-text-decoration:underline;--framer-paragraph-spacing:0px;--extracted-1w3ko1f:rgb(242, 228, 34);--extracted-5dqwso:rgb(242, 228, 34);--extracted-1sfm3sg:rgb(242, 228, 34);transform:none" data-framer-component-type="RichText" class="framer-si2tk6 rich-text-wrapper">
+                                    <p style="--framer-line-height:2em;">
+                                        <span style="--framer-font-family:&quot;UhBee KeongKeong Regular&quot;, serif; --framer-font-style:normal; --framer-font-weight:400; --font-selector:Q1VTVE9NO1VoQmVlIEtlb25nS2VvbmcgUmVndWxhcg==; --framer-text-color:var(--extracted-1w3ko1f); --framer-font-size:29px;">"내가 너의</span>
+                                    </p>
+                                    <p style="--framer-line-height:2em;">
+                                        <span style="--framer-font-family:&quot;UhBee KeongKeong Regular&quot;, serif; --framer-font-style:normal; --framer-font-weight:400; --font-selector:Q1VTVE9NO1VoQmVlIEtlb25nS2VvbmcgUmVndWxhcg==; --framer-text-color:var(--extracted-5dqwso); --framer-font-size:29px;">비생일을</span>
+                                    </p>
+                                    <p style="--framer-line-height:2em;">
+                                        <span style="--framer-font-family:&quot;UhBee KeongKeong Regular&quot;, serif; --framer-font-style:normal; --framer-font-weight:400; --font-selector:Q1VTVE9NO1VoQmVlIEtlb25nS2VvbmcgUmVndWxhcg==; --framer-text-color:var(--extracted-1sfm3sg); --framer-font-size:29px;">축하해줄게!"</span>
+                                    </p>
+                                </div>
+                                <div class="framer-d04yhy" name="박스" data-framer-name="박스">
+                                    <div style="position:absolute;pointer-events:none;user-select:none;border-radius:inherit;top:0;right:0;bottom:0;left:0" data-framer-background-image-wrapper="true">
+                                        <div style="display:contents;border-radius:inherit;pointer-events:none">
+                                            <img src="https://framerusercontent.com/images/zurraKs6UZLsbDZwyY3j3kcNP4M.png" alt="" srcset="https://framerusercontent.com/images/zurraKs6UZLsbDZwyY3j3kcNP4M.png?scale-down-to=512 512w, https://framerusercontent.com/images/zurraKs6UZLsbDZwyY3j3kcNP4M.png 937w" sizes="calc(236px)" style="pointer-events:none;user-select:none;display:block;width:100%;height:100%;border-radius:inherit;object-position:center;object-fit:cover;image-rendering:auto">
+                                        </div>
+                                    </div>
+                                    <div style="outline:none;display:flex;flex-direction:column;justify-content:flex-start;flex-shrink:0;white-space:pre;--framer-paragraph-spacing:0px;--extracted-1w3ko1f:rgb(242, 228, 34);transform:none" data-framer-component-type="RichText" class="framer-1wdfkx9 rich-text-wrapper">
+                                        <p style="--framer-text-alignment:center;">
+                                            <span style="--framer-font-family:&quot;Noto Sans KR&quot;, sans-serif; --framer-font-style:normal; --framer-font-weight:500; --font-selector:R0Y7Tm90byBTYW5zIEtSLTUwMA==; --framer-text-color:var(--extracted-1w3ko1f); --framer-font-size:22px;">비생일 카드 보내기</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="overlay"></div>
+                        </div>
+                    </div>
+                <div id="svg-templates" style="position: absolute; overflow: hidden; top: 0; left: 0; width: 0; height: 0">
+            </div>
             </div>
         )
     }
