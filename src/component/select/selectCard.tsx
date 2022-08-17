6ee1a1d975ffbbddfd62655e41cc5323d767880e
MinY-Lee <leemy08@naver.com>
2022-08-17 11:20:55 +0900

    [ADD] 이미지 추가, selectCard tsx,css 추가

---
+++ src/component/select/selectCard.tsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import '../../static/select.css';
+{/* <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link> */}
+
+
+class selectCard extends React.Component<{}, any> {
+    constructor(props: any){
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                레터 받기
+            </div>
+        )
+    }
+}
+
+export default selectCard;
