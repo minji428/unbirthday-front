99aee4ac53d508b8e0c08746052975495041b76e
minji428 <alswl980428@gmail.com>
2022-09-05 21:17:59 +0900

    주석 삭제

--- src/component/letter/cardSend2.tsx
+++ src/component/letter/cardSend2.tsx
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import '../../static/cardsend2.css';
-{/* <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link> */}
-
 
 class cardSend2 extends React.Component<{}, any> {
     constructor(props: any){
