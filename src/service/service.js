3e09bd2f17f673706e85618a18628e6fc5d3a2ca
Minji Park <83818241+minji428@users.noreply.github.com>
2022-11-19 00:35:14 +0900

    Update service.js

--- src/service/service.js
+++ src/service/service.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import * as httpStatus from '../util/httpErrorStatusMsgUtil'
-let defaultUrl = 'http://localhost:8080'
+let defaultUrl = 'http://3.37.70.125:8080'
 
 export function anyService(requestURL, requestMethod, callbackFunc, param) {
     console.log(requestURL)
     var axiosBody = {
-        // url : defaultUrl + requestURL,
-        url : requestURL,
+        url : defaultUrl + requestURL,
+        // url : requestURL,
         method : requestMethod,
     }
 
